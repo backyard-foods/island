4685947d9672bf4ec89cbf864ce7459febc6add4
Milan Kodali <milankodali@gmail.com>
2024-09-09 17:29:01 -0400

    Get state from backend

--- island/src/byf_api_client.py
+++ island/src/byf_api_client.py
@@ -8,15 +8,14 @@ class BYFAPIClient:
         self.anon_key = os.environ['ANON_KEY']
         self.user = os.environ['BYF_USER']
         self.password = os.environ['BYF_PW']
+        self.device_id = os.environ['RESIN_DEVICE_UUID']
+        self.device_name = os.environ['BALENA_DEVICE_NAME_AT_INIT']
+        self.device_type = 'island'
         self.access_token = None
-        self.token_expiry = 0  # Add this line
+        self.token_expiry = 0 
+        self.state = None
 
     def authenticate(self):
-        print("Authenticating with BYF API")
-        print(f"API URL: {self.api_url}")
-        print(f"Anon Key: {self.anon_key}")
-        print(f"User: {self.user}")
-        print(f"Password: {self.password}")
         auth_url = f"{self.api_url}/auth/v1/token?grant_type=password"
         auth_data = {
             "email": self.user,
@@ -36,12 +35,39 @@ class BYFAPIClient:
             print(f"Authentication failed: {e}")
             raise
 
+    def get_state(self):
+        state_url = f"{self.api_url}/functions/v1/state"
+        state_headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json"
+        }
+        state_url_params = {
+            "deviceId": self.device_id,
+            "deviceName": self.device_name,
+            "deviceType": self.device_type
+        }
+        # Check if the token is valid, if not, authenticate
+        if not self.is_token_valid():
+            self.authenticate()
+        
+        try:
+            state_response = requests.get(state_url, headers=state_headers, params=state_url_params)
+            state_response.raise_for_status()
+            self.state = state_response.json()
+            return self.state
+            
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to get device state: {e}")
+            raise
+
     def is_token_valid(self):
         return self.access_token and time() < self.token_expiry
 
     def notify_print_success(self, order):
         if not self.is_token_valid():
             self.authenticate()
+
+        self.get_state()
 
         notify_url = f"{self.api_url}/functions/v1/print"
         notify_headers = {
@@ -52,10 +78,6 @@ class BYFAPIClient:
             "order": order,
         }
 
-        print(f"Notifying backend of print completion for order: {order}")
-        print(f"Notify URL: {notify_url}")
-        print(f"Notify Headers: {notify_headers}")
-        print(f"Notify Body: {notify_body}")
         try:
             notify_response = requests.post(notify_url, json=notify_body, headers=notify_headers)
             notify_response.raise_for_status()
