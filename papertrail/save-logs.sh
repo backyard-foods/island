7863b9b642890b8476f85f742ea363d86f8c7f5b
Milan Kodali <milankodali@gmail.com>
2025-01-21 12:57:11 -0500

    Periodically refresh container_map

--- papertrail/save-logs.sh
+++ papertrail/save-logs.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-# Query supervisor state endpoint to map containerId to serviceName
-state_json=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v2/state/status?apikey=$BALENA_SUPERVISOR_API_KEY")
-
+# map containerId to serviceName
 declare -A container_map
-while IFS= read -r container; do
-	container_id=$(echo "$container" | jq -r '.containerId' | cut -c1-12)
-	service_name=$(echo "$container" | jq -r '.serviceName')
-	container_map["$container_id"]="$service_name"
-done < <(echo "$state_json" | jq -c '.containers[]')
 
-while true;
-do
+refresh_container_map() {
+	state_json=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v2/state/status?apikey=$BALENA_SUPERVISOR_API_KEY")
+	while IFS= read -r container; do
+		container_id=$(echo "$container" | jq -r '.containerId' | cut -c1-12)
+		service_name=$(echo "$container" | jq -r '.serviceName')
+		container_map["$container_id"]="$service_name"
+	done < <(echo "$state_json" | jq -c '.containers[]')
+}
+
+# populate container_map
+refresh_container_map
+
+# refresh container_map every 60 seconds
+while true; do
+	refresh_container_map
+	sleep 60
+done &
+
+# loop to process logs
+while true; do
 	curl -X POST -H "Content-Type: application/json" --no-buffer --data '{"follow":true,"all":true,"format":"short"}' "$BALENA_SUPERVISOR_ADDRESS/v2/journal-logs?apikey=$BALENA_SUPERVISOR_API_KEY" \
-	| while read -r line ;
-		do
-			container_id=$(echo "$line" | grep -oE '[a-f0-9]{12}')
-			
-			# Check if the container_id is not empty and exists in the map
-			if [[ -n "$container_id" && -n "${container_map[$container_id]}" ]]; then
-				# Replace the container ID with the service name
-				line=${line//$container_id/${container_map[$container_id]}}
-			fi
-			
-			# Send the modified log line to papertrail
-			curl -s -u :$PAPERTRAIL_TOKEN -H "content-type:text/plain" -d "${line}" "https://logs.collector.solarwinds.com/v1/log";
-		done
+	| while IFS= read -r line; do
+		container_id=$(echo "$line" | grep -oE '[a-f0-9]{12}')
+		
+		# check if container_id is not empty and exists in the map
+		if [[ -n "$container_id" && -n "${container_map[$container_id]}" ]]; then
+			# replace container_id with service_name
+			line=${line//$container_id/${container_map[$container_id]}}
+		fi
+		
+		# send modified log line to papertrail
+		curl -s -u :$PAPERTRAIL_TOKEN -H "content-type:text/plain" -d "${line}" "https://logs.collector.solarwinds.com/v1/log";
+	done
 done
