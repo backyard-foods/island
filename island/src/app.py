201ad6fc8f9c03eef4fb5e07e4580e66378b27f5
Milan Kodali <milankodali@gmail.com>
2025-06-28 15:34:54 -0400

    Reduce print_receipt_cache time

--- island/src/app.py
+++ island/src/app.py
@@ -25,7 +25,7 @@ SERVICE_STOP_START_CHECK_INTERVAL_S = 1
 SUCCESS_SOUND = pygame.mixer.Sound('success2.wav')
 SUCCESS_SOUND.set_volume(1.0)
 
-print_receipt_cache = TTLCache(maxsize=100, ttl=1.5 * 60)
+print_receipt_cache = TTLCache(maxsize=100, ttl=45)
 print_label_cache = TTLCache(maxsize=100, ttl=10)
 
 print_receipt_lock = threading.Lock()
